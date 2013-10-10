a775f7918f7f0063c6e570667902504f2fd06d90
Vladyslav Chernobai <vladyslav.chernobai@masterofcode.com>
2013-10-11 00:11:50 +0300

    first tests

---
+++ test/src/test/GroovyFirstTest.groovy
@@ -0,0 +1,20 @@
+package test
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: angelys
+ * Date: 10/10/13
+ * Time: 11:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class GroovyFirstTest extends GroovyTestCase {
+
+    void testSome(){
+        assert (1..3).collect({3}) == [3, 3, 3]
+
+        assert [1, 2, 4].contains(4);
+
+        shouldFail { 3/0 }
+    }
+
+}
