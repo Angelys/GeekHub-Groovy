81a48ff7f0a43da7928bf321a8b4c03ce9bbfc53
Vladyslav Chernobai <vladyslav.chernobai@masterofcode.com>
2014-01-26 16:28:11 +0200

    refactor

--- Geekhub-grails/grails-app/views/pushNotification/index.gsp
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>Notifications</title>
-        <meta name="layout" content="notification" />
-    </head>
-    <body>
-        <g:render template="notification"/>
-    </body>
-</html>
--- Geekhub-grails/target/work/plugins/android-gcm-0.2/src/groovy/es/osoco/android/gcm/exception/ApiKeyNotFoundException.groovy
+++
@@ -1,8 +0,0 @@
-package es.osoco.android.gcm.exception
-
-class ApiKeyNotFoundException extends Exception {
-
-    ApiKeyNotFoundException(String apiKeyPropertyName) {
-        super("ApiKeyNotFoundException: Api Key should be in the Config.groovy file as $apiKeyPropertyName.")
-    }
-}
